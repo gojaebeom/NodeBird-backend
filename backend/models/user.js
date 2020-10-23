533bfed72ae7884ade89f6d5d23fd30d837ff25c
gojaebeom <const.gjb@gmail.com>
2020-10-23 23:20:21 +0900

    done!

--- backend/models/user.js
+++ backend/models/user.js
@@ -21,7 +21,7 @@ module.export = (sequelize, DataTypes) => {
         charset : 'utf8',
         collate: 'utf8_general_ci', //한글 저장
     });
-    User.associate = (db) => {
+    User.associate = (db) => { 
         db.User.hasMany(db.Post);
         db.User.hasMany(db.comment);
         db.User.belongsToMany(db.Post,{through:'Like', as : 'Liked'});
